b7ec81cc24da13dd461f81b9653e027f62c66ccd
ocueye2 <carsonmayn@fuse.net>
2024-09-18 15:46:09 +0000

    added logs

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ if __name__ == '__main__':
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',  # Bind to all available network interfaces
         'server.socket_port': 8080,
-        'log.screen': False  
+        'log.screen': True  
     })
     cherrypy.quickstart(webui())
