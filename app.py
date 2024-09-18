0deddae086f084cf9122ce4ccf69679f81bafd55
ocueye2 <carsonmayn@fuse.net>
2024-09-18 00:15:51 +0000

    please

---
+++ app.py
@@ -0,0 +1,67 @@
+import cherrypy
+import os
+import sys
+
+base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+def load(file):
+    try:
+        with open(os.path.join(base_dir, file)) as f:
+            return f.read()
+    except FileNotFoundError as e:
+        print("fnf")
+        return ""
+
+
+class webui(object):
+
+    @cherrypy.expose
+    def index(self):
+        return load("bake/index.html")
+
+    @cherrypy.expose
+    def projects(self):
+        return load("bake/projects.html")
+
+    @cherrypy.expose
+    def contact(self):
+        return load("bake/contact.html")
+
+    @cherrypy.expose
+    def servers(self):
+        return load("bake/servers.html")
+
+    @cherrypy.expose
+    def submit(self,phone="empty",email="empty",message="empty",pref="",name=""):
+        global contact
+        if phone == "empty":
+            return load("bake/error.html")
+        else:
+            f = open(f"{base_dir}/contact.html","a")
+            f.write(f"""
+            {{
+                "name":"{name}",
+                "pref":"{pref}",
+                "email":"{email}",
+                "phone":"{phone}",
+                "message":"{message}",
+            }},
+            """)
+            f.close()
+
+            return load("bake/submit.html")
+            
+
+    def default(self, attr='abc'):
+        return load("bake/404.html")
+    default.exposed = True
+
+
+
+if __name__ == '__main__':
+    cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',  # Bind to all available network interfaces
+        'server.socket_port': 1232,
+        'log.screen': False  
+    })
+    cherrypy.quickstart(webui())
